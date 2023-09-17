35310c055a0a65d2abef2412daa54972f80608f2
AAVinogradov <aavinogradov@bk.ru>
2023-09-17 20:49:49 +0300

    Вывод степеней двойки. Добавлено решение задачи 14 семинара 2

--- S2DZ14.py
+++ S2DZ14.py
@@ -1,1 +1,11 @@
 # Задача 14: Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
+
+count = int(input("Введите число "))
+maxCountInt = 2147483647
+if count > maxCountInt:
+    print("Превышено максимальное значение типа данных Int")
+r = range(0, count, 1)
+for x in r:
+    p = 2 ** x
+    if p < count:
+        print(p)
