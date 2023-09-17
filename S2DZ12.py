259f491912c86ec1558321501fe7ecb951b7cf3e
AAVinogradov <aavinogradov@bk.ru>
2023-09-17 18:10:48 +0300

    Нахождение чисел при известной сумме и произведению. Добавлено решение задачи 12 семинара 2

---
+++ S2DZ12.py
@@ -0,0 +1,16 @@
+# Задача 12: Петя и Катя – брат и сестра. Петя – студент, а Катя – школьница. Петя помогает Кате по математике. Он задумывает два натуральных числа X и Y (X,Y≤1000), а Катя должна их отгадать. Для этого Петя делает две подсказки. Он называет сумму этих чисел S и их произведение P. Помогите Кате отгадать задуманные Петей числа.
+
+numberX = int(input("Введите число X "))
+numberY = int(input("Введите число Y "))
+temp = 0
+if numberX > numberY:
+    temp = numberY
+    numberY = numberX
+    numberX = temp
+if numberX > 1000 or numberY > 1000:
+    print("Не верно заданы входные значения!")
+else:
+    for x in range(numberX):
+        for y in range(numberY):
+            if numberX == x + y and numberY == x * y:
+                print(f"Первое число = {x}, второе число = {y}")
