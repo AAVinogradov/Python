ee808f11dac13a1e1cd5dcda01ad0670ce0cd49a
AAVinogradov <aavinogradov@bk.ru>
2023-10-12 22:01:45 +0300

    добавил еще решения задачи 7

--- S1Z7.py
+++ S1Z7.py
@@ -4,9 +4,15 @@
 # Input: 2016
 # Output: YES
 
-year = int(input("Введите год "))
+# year = int(input("Введите год "))
 
-if year % 4 == 0 and year % 100 != 0:
-    print("Yes")
+# if year % 4 == 0 and year % 100 != 0:
+#     print("Yes")
+# else:
+#     print("No")
+    
+year = int(input())
+if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+    print('YES')
 else:
-    print("No")
+    print('NO')
